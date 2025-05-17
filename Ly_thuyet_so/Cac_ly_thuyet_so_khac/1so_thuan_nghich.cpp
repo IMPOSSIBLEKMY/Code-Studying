b0a2a11cab5cefd50fc7f9f27f6dfae235e29621
IMPOSSIBLEKMY <impossiblekmy12345@gmail.com>
2025-05-17 01:43:18 -0700

    o

--- Ly_thuyet_so/Cac_ly_thuyet_so_khac/1so_thuan_nghich.cpp
+++ Ly_thuyet_so/Cac_ly_thuyet_so_khac/1so_thuan_nghich.cpp
@@ -1,45 +1,50 @@
 #include <bits/stdc++.h>
-
-#define ll long long
-
 using namespace std;
 
-bool so_thuan_nghich(ll n)
+// @ Long long có giới hạn từ -9223372036854775808 -> 9223372036854775807
+#define ll long long
+
+// ! Kiểm tra số thuận nghịch - đúng hay sai
+
+// # ĐN         : Số đọc từ trái sang phải hay từ phải sang trái đều giống nhau
+// # VD         : 0, 3, 121, 1331, 9223372036302733229
+// # Độ phức tạp: O(log10(n))
+
+// $ 1. Không xét số âm
+// $ 2. Ý tưởng đảo ngược số:
+// $        VD: 357 => 0*10 + 7  = 7
+// $                => 7*10 + 5  = 75
+// $                => 75*10 + 3 = 753
+
+bool isPalindromicNumber(ll num)
 {
-    if (n < 0)
+    if (num < 0)
     {
-        return 0;
+        return false;
     }
 
-    ll n1 = n;
-    ll tong = 0;
+    ll copyNum = num;
 
-    while (n)
+    ll reversedNum = 0;
+    while (num > 0)
     {
-        tong = tong * 10 + n % 10;
-        n /= 10;
+        reversedNum = reversedNum * 10 + num % 10;
+        num /= 10;
     }
 
-    if (tong == n1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    return (reversedNum == copyNum);
 }
 
 int main()
 {
-    ll n;
-    cin >> n;
+    ll num;
+    cin >> num;
 
     time_t start, end;
     time(&start);
     ios_base::sync_with_stdio(false);
 
-    if (so_thuan_nghich(n))
+    if (isPalindromicNumber(num))
     {
         cout << "YES" << endl;
     }
--- Ly_thuyet_so/Cac_ly_thuyet_so_khac/2to_hop_chap_k_cua_n.cpp
+++ Ly_thuyet_so/Cac_ly_thuyet_so_khac/2to_hop_chap_k_cua_n.cpp
@@ -1,22 +1,31 @@
 #include <bits/stdc++.h>
 
+// @ Long long có giới hạn từ -9223372036854775808 -> 9223372036854775807
 #define ll long long
 
 using namespace std;
 
-ll to_hop_chap_k_cua_n(int n, int k)
+// ! Tính tổ hợp chập k của n - trả về kết quả
+
+// # ĐN         : $$\color{#fffb00}\mathrm{C}_{n}^{k}=\frac{n!}{k!(n-k)!}=\frac{n(n-1)(n-2)...(n-k+1)}{k!}$$
+
+// # VD         : 6C3 = 20, 6C2 = 15
+// # Độ phức tạp: O(k)
+
+// $ 1. Không xét số âm, không xét k > n
+// $ 2. Xét các trường hợp đặc biệt như:
+// $        k = n hay k = 0
+// $        k = 1 hay k = n - 1
+// $ 3. Tính công thức nCk theo cách rút gọn
+
+ll calculateCombination(int n, int k)
 {
     if (k > n || n < 0 || k < 0)
-    {
-        return -1;
-    }
-
-    if (n == 0 && k == 0)
     {
         return 0;
     }
 
-    if (n == k || k == 0)
+    if (k == 0 || k == n)
     {
         return 1;
     }
@@ -28,15 +37,14 @@ ll to_hop_chap_k_cua_n(int n, int k)
 
     ll tich1 = 1;
     ll tich2 = 1;
-
     for (int i = 1; i <= k; i++)
     {
         tich1 *= n;
-        n--;
         tich2 *= i;
+        n--;
     }
 
-    return tich1 / tich2;
+    return (tich1 / tich2);
 }
 
 int main()
@@ -48,7 +56,7 @@ int main()
     time(&start);
     ios_base::sync_with_stdio(false);
 
-    cout << to_hop_chap_k_cua_n(n, k) << endl;
+    cout << calculateCombination(n, k) << endl;
 
     time(&end);
     double time_taken = double(end - start);
