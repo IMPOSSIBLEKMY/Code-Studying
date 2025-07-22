84e1fe7a87eec51d3faf8d93c04d5061bc263d25
IMPOSSIBLEKMY <impossiblekmy12345@gmail.com>
2025-07-21 22:59:12 -0700

    o

--- Bài Tập Đệ Quy/1_Tong_Mot.cpp
+++ Bài Tập Đệ Quy/1_Tong_Mot.cpp
@@ -1,23 +1,27 @@
 #include <bits/stdc++.h>
-using namespace std;
 
 #define ll long long
 
+using namespace std;
+
 // Đề bài:
-// Viết một chương trình tính tổng các số nguyên từ 1 đến n (n ≥ 1) bằng phương pháp đệ quy.
+// Viết một chương trình tính tổng các số nguyên từ 1 đến n bằng phương pháp đệ quy.
 
-
+ll tong_1(int n)
+{
+    if()
+}
 
 int main()
 {
-    ll number;
-    cin >> number;
+    int n;
+    cin >> n;
 
     time_t start, end;
     time(&start);
     ios_base::sync_with_stdio(false);
 
-
+    cout << tong_1(n) << endl;
 
     time(&end);
     double time_taken = double(end - start);
